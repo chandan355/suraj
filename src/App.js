9aa7f1c22a21a5cc62817f3ac44fdac06094707e
chandan355 <chandansharma13532@gmail.com>
2025-09-18 16:46:50 +0530

    add props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
 import './App.css';
-import Card from './my all project/card';
-import Props from './my all project/Header';
+
+
 
 
 
 function App() {
-  return (
-   <div>
-<Props/>
+  <div>
     </div>
-  );
+  
 }
 
 export default App;
---
+++ src/my all project/props.jsx
@@ -0,0 +1,11 @@
+function Props({email,info})
+{
+    return (
+   <div>
+   
+<h1>welcome to Props<br></br>{email}<br></br>{info.name}</h1>
+    </div>
+  );
+   
+}
+export default Props;
