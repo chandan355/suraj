ee7d63afc2ff466f10b4f8b4258b7896c46a2d96
chandan355 <chandansharma13532@gmail.com>
2025-09-27 12:40:27 +0530

    Update card.jsx

--- src/my all project/card.jsx
+++ src/my all project/card.jsx
@@ -3,7 +3,15 @@ function Card()
     return(
  
         <div className="cardItem">
+            <h1>Make a Card Item</h1>
             Card
+            <lable>name</lable>
+            <input type="text"/>
+            <lable>Email</lable>
+            <input type="email"/>
+            <lable>password</lable>
+            <input type="password"/>
+            
         </div>
        
     )
